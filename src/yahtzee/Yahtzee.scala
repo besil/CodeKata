0f4f5ca874ebdfc62e7481ec68f0b3ff1932bff1
besil <s.bernardinello@reply.it>
2015-04-29 10:55:47 +0200

    Finished!

--- src/yahtzee/Yahtzee.scala
+++ src/yahtzee/Yahtzee.scala
@@ -5,21 +5,37 @@ class Yahtzee {
   
   val ones = makeSimpleRule(1)(_)
   val twos = makeSimpleRule(2) _
+  val threes = makeSimpleRule(3) _
+  val fours = makeSimpleRule(4) _
+  val fives = makeSimpleRule(5) _
+  val sixes = makeSimpleRule(6) _
   
-  def pair = (l: List[Int]) => {
+  val pair = (l: List[Int]) => {
    l.map { x => (x, l.count(_==x)) }.maxBy(x => x._1) match {
      case (x, y) => x * y
    }
   }
   
-  def groupKind(n: Int)(l:List[Int]): Int = {
-    l.groupBy(x=>x).filter(x => x._2.size==n).values.flatten.sum
+  private def groupKind(n: Int)(l:List[Int]): Int = l.groupBy(x=>x).filter(x => x._2.size==n).values.flatten.sum
+  
+  val twopair = groupKind(2) _
+  val threekind = groupKind(3) _
+  val fourkind = groupKind(4) _
+  
+  private def straight(range: List[Int])(l:List[Int]) = l==range match {
+    case true => range.sum
+    case _ => 0
   }
   
-//  def twopair = (l: List[Int]) => l.groupBy(x => x).filter(x => x._2.size==2).values.flatten.sum
-  def twopair = groupKind(2) _
-//  def threekind = groupKind(3) _
-  def threekind = (l: List[Int]) => {
-    l.groupBy { x => x }.filter(_._2.size==3).values.flatten.sum
+  val smallstraight = straight(1 to 5 toList)(_)
+  val largestraight = straight(2 to 6 toList)(_)
+  
+  val fullhouse = (l: List[Int]) => if(twopair(l)>0 && threekind(l)>0) l.sum else 0
+  
+  val yahtzee = (l: List[Int]) => l.map(x => x/l(0)).sum match {
+    case 5 => 50
+    case _ => 0
   }
+  
+  val chance = (l: List[Int]) => l.sum
 }
--- src/yahtzee/YahtzeeTest.scala
+++ src/yahtzee/YahtzeeTest.scala
@@ -27,5 +27,27 @@ class YahtzeeTest {
   @Test
   def threekind() = assertEquals(9, yahtzee.threekind(List(3, 3, 3, 4, 5)))
   
+  @Test
+  def fourkind() = assertEquals(8, yahtzee.fourkind(List(2, 2, 2, 2, 5)))
   
+  @Test
+  def smallstraight() = assertEquals(15, yahtzee.smallstraight(List(1,2,3,4,5)))
+  
+  @Test
+  def largestraight() = assertEquals(20, yahtzee.largestraight(List(2,3,4,5,6)))
+  
+  @Test
+  def fullhouse() = {
+    assertEquals(8, yahtzee.fullhouse(List(1,1,2,2,2)))
+    assertEquals(0, yahtzee.fullhouse(List(4,4,4,4,4)))
+  }
+  
+  @Test
+  def yahtzeeTest() = assertEquals(50, yahtzee.yahtzee(List(2,2,2,2,2)))
+  
+  @Test
+  def chanceTest() = {
+    assertEquals(5*5, yahtzee.chance( (for(i <- 1 to 5) yield 5) toList))
+    assertEquals(5, yahtzee.chance(List(1,1,1,1,1)))
+  }
 }
